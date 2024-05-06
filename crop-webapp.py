55799bf350d5deecd6e8ae773a8e4493de406082
Riya Samantaray <110122876+Riyasamantaray23@users.noreply.github.com>
2024-05-06 09:46:15 +0530

    Update crop-webapp.py

--- crop-webapp.py
+++ crop-webapp.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 import pickle
 from PIL import Image
 
@@ -11,28 +10,31 @@ RF_model=pickle.load(open('RF_model.pkl','rb'))
 def classify(answer):
     return answer[0]+" is the best crop for cultivation here."
 
-
 def main():
     st.title("GraMitra (Crop Recommender)...")
     image=Image.open('cc.jpg')
     st.image(image)
     html_temp = """
-    <div style="background-color:teal; padding:10px">
-    <h2 style="color:white;text-align:center;">Find The Most Suitable Crop</h2>
+    <div style="background-color:#85c532; padding:10px">
+    <h2 style="color:#d59944;text-align:center;">Find the most suitable crop using GraMitra's crop recommender.</h2>
     </div>
     """
 
     st.markdown(html_temp, unsafe_allow_html=True)
-    activities=['Naive Bayes (The Best Model)','Logistic Regression','Decision Tree','Random Forest']
-    option=st.sidebar.selectbox("which model would you like to use?",activities)
+    
+    # Add logo to the top left corner
+    st.sidebar.image('your_logo.png', width=200)
+
+    activities=['Naive Bayes','Logistic Regression','Decision Tree','Random Forest']
+    option=st.sidebar.selectbox("Which model would you like to use?",activities)
     st.subheader(option)
-    sn=st.slider('NITROGEN (N)', 0.0, 150.0)
-    sp=st.slider('PHOSPHOROUS (P)', 0.0, 150.0)
-    pk=st.slider('POTASSIUM (K)', 0.0, 210.0)
-    pt=st.slider('TEMPERATURE', 0.0, 50.0)
-    phu=st.slider('HUMIDITY', 0.0, 100.0)
-    pPh=st.slider('Ph', 0.0, 14.0)
-    pr=st.slider('RAINFALL', 0.0, 300.0)
+    sn=st.slider('NITROGEN (N) in (kg/ha)', 0.0, 150.0)
+    sp=st.slider('PHOSPHOROUS (P) in (kg/ha)', 0.0, 150.0)
+    pk=st.slider('POTASSIUM (K)in (kg/ha)', 0.0, 210.0)
+    pt=st.slider('TEMPERATURE in °C', 0.0, 50.0)
+    phu=st.slider('HUMIDITY in %', 0.0, 100.0)
+    pPh=st.slider('ph', 0.0, 14.0)
+    pr=st.slider('RAINFALL in mm', 0.0, 300.0)
     inputs=[[sn,sp,pk,pt,phu,pPh,pr]]
     if st.button('Classify'):
         if option=='Logistic Regression':
@@ -43,7 +45,6 @@ def main():
             st.success(classify(NaiveBayes_model.predict(inputs)))
         else:
             st.success(classify(RF_model.predict(inputs)))   
-
 
 if __name__=='__main__':
     main()
